3ebc9e4a86400c2c405fb2dba7adcb59347a02a4
erison&erick <erisonsuzuki@gmail.com>
2016-11-21 21:49:48 -0200

    User visit home and goes to controller index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-	resources :equipment, only: [:new, :create, :show]
+	root to: 'home#index'
+
+  resources :equipment, only: [:new, :create, :show]
   resources :customers, only: [:new, :create, :show]
 
   resources :contracts, only: [:index, :new, :create, :show] do
---
+++ spec/features/user_visit_admin_page_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User visit admin page' do 
+
+  scenario 'successfully' do
+
+    visit root_path
+
+    click_on 'Painel Administrativo'
+
+    within('nav') do
+      expect(page).to have_xpath(".//a[@href = '#{new_contract_path}']")
+      expect(page).to have_xpath(".//a[@href = '#{new_customer_path}']")
+      expect(page).to have_xpath(".//a[@href = '#{new_equipment_path}']")
+    end
+
+  end
+  
+end
