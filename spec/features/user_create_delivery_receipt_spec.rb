35fe9c3d6d070b2946d566bc5086592bd6198308
erison&erick <erisonsuzuki@gmail.com>
2016-11-21 20:23:10 -0200

    User visit delivery receipt page already exist

--- spec/features/user_create_delivery_receipt_spec.rb
+++ spec/features/user_create_delivery_receipt_spec.rb
@@ -29,6 +29,31 @@ feature 'user create delivery receipt' do
     visit delivery_receipt_contract_path(1)
 
     expect(page).to have_current_path(contracts_path)
-    expect(page).to have_content('Contrato inválido')
+    expect(page).to have_content 'Contrato inválido'
+  end
+
+  scenario 'User visit contract page to see delivery receipt already exist' do
+    contract = create(:contract)
+    delivery_receipt = create(:delivery_receipt, contract: contract)
+
+    visit contracts_path
+
+    expect(page).to have_content 'Visualizar'
+  end
+
+  scenario "User click on 'Visualizar' and visit delivery receipt page" do
+    contract = create(:contract)
+    delivery_receipt = create(:delivery_receipt, contract: contract)
+
+    visit contracts_path
+
+    click_on 'Visualizar'
+
+    expect(page).to have_content('Recibo de Entrega')
+    expect(page).to have_content(contract.contact)
+    expect(page).to have_content(contract.rental_period)
+    expect(page).to have_content(contract.delivery_address)
+    expect(page).to have_content(contract.delivery_receipt.created_at)
+    expect(page).to have_content(contract.delivery_receipt.contract_id)
   end
 end
