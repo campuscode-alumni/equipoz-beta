32f9e626ecf46bbf0c1cf18d5536a3d33764f918
erison&erick <erisonsuzuki@gmail.com>
2016-11-23 19:49:11 -0200

    Create Seed - Fast way to create mass

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Customer.destroy_all
+Equipment.destroy_all
+
+customer = Customer.create(name: 'Rodrigo',
+                         legal_name: 'Clockwork',
+                         customer_type: '0',
+                         document: '777777777-99',
+                         fiscal_number: '5688275429',
+                         contact_name: 'Paula',
+                         phone_number: '1406-1406',
+                         email: 'rcecosta@gmail.com',
+                         address: 'Explanada dos ministérios, 33')
+
+categoria = Category.create(name: 'Furadeira')
+
+equipamento = Equipment.create(category: categoria,
+                                serial_number: "AGCBRA001",
+                                acquisition_date: "2016-11-17 20:26:23",
+                                replacement_value: 100.5,
+                                usage_limit: 10,
+                                description: "Makita 120v")
