7305ba53adeb17c1c333c42055cfae6440832d72
erison&erick <erisonsuzuki@gmail.com>
2016-11-22 22:08:51 -0200

    refactor contract

--- spec/features/user_visit_contracts_page_spec.rb
+++ spec/features/user_visit_contracts_page_spec.rb
@@ -4,29 +4,27 @@ feature 'User visit contracts page' do
   scenario 'successfully' do
     equipment = create(:equipment)
 
-    contract_01 = create(:contract, equipment_ids: [equipment.id])
+    contract_01 = create(:contract, equipment: [equipment])
     contract_02 = create(:contract, total_amount:450.90, equipment: [equipment])
 
     visit contracts_path
 
+
     expect(page).to have_content('Contratos')
-    expect(page).to have_content(contract_01.equipment.first.description)
-    expect(page).to have_content(contract_01.rental_period)
-    expect(page).to have_content(contract_01.delivery_address)
-    expect(page).to have_content(contract_01.contact)
-    expect(page).to have_content(contract_01.payment_method)
+    expect(page).to have_content(contract_01.id)
+    expect(page).to have_content(contract_01.customer.name)
     expect(page).to have_content(contract_01.amount)
     expect(page).to have_content(contract_01.discount)
     expect(page).to have_content(contract_01.total_amount)
 
-    expect(page).to have_content(contract_02.equipment.first.description)
-    expect(page).to have_content(contract_02.rental_period)
-    expect(page).to have_content(contract_02.delivery_address)
-    expect(page).to have_content(contract_02.contact)
-    expect(page).to have_content(contract_02.payment_method)
+
+
+    expect(page).to have_content(contract_02.id)
+    expect(page).to have_content(contract_02.customer.name)
     expect(page).to have_content(contract_02.amount)
     expect(page).to have_content(contract_02.discount)
     expect(page).to have_content(contract_02.total_amount)
+
 
   end
 
